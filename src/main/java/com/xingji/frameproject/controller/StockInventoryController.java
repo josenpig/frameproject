9dd57346d503ad64fa81e8256355f8e2bc752c82
hdr <957176817@qq.com>
2021-06-10 20:40:45 +0800

    no message

--- src/main/java/com/xingji/frameproject/controller/StockInventoryController.java
+++ src/main/java/com/xingji/frameproject/controller/StockInventoryController.java
@@ -183,37 +183,37 @@ public class StockInventoryController {
 
 
 
-    /**
-     * 新增销售订单
-     * @param add 单据信息
-     * @param type 是否为草稿
-     * @return 订单id
-     */
-    @RequestMapping("/stockInventory/add/{type}")
-    public AjaxResponse add(@PathVariable("type") int type,@RequestBody String add){
-        JSONObject jsonObject = JSONObject.parseObject(add);
-        String one = jsonObject.getString("order");
-        StockInventory order = JSON.parseObject(one, StockInventory.class);
-        String two = jsonObject.getString("orderdetails");
-        List<StockInventoryDetails> orderdetails= JSONArray.parseArray(two, StockInventoryDetails.class);
-        order.setInventorystate(type);
-        order.setOrderState(0);
-        order.setDeliveryState(0);
-        order.setAdvance(0.00);
-        //添加销售订单单信息
-        for(int i=0;i<orderdetails.size();i++){
-            orderdetails.get(i).setOrderId(order.getOrderId());
-        }
-        sos.insert(order);
-        sods.insertBatch(orderdetails);
-        //订单生成减去预计库存数量
-        if(type == 0) {
-            List<SaleOrderDetails> orderDetails=sods.queryById(order.getOrderId());
-            for(SaleOrderDetails sod:orderDetails){
-                bos.expectreduce(sod.getProductId(),sod.getDepot(),sod.getProductNum());
-            }
-        }
-        return AjaxResponse.success(order.getOrderId());
-    }
+//    /**
+//     * 新增销售订单
+//     * @param add 单据信息
+//     * @param type 是否为草稿
+//     * @return 订单id
+//     */
+//    @RequestMapping("/stockInventory/add/{type}")
+//    public AjaxResponse add(@PathVariable("type") int type,@RequestBody String add){
+//        JSONObject jsonObject = JSONObject.parseObject(add);
+//        String one = jsonObject.getString("order");
+//        StockInventory order = JSON.parseObject(one, StockInventory.class);
+//        String two = jsonObject.getString("orderdetails");
+//        List<StockInventoryDetails> orderdetails= JSONArray.parseArray(two, StockInventoryDetails.class);
+//        order.setInventorystate(type);
+//        order.setOrderState(0);
+//        order.setDeliveryState(0);
+//        order.setAdvance(0.00);
+//        //添加销售订单单信息
+//        for(int i=0;i<orderdetails.size();i++){
+//            orderdetails.get(i).setOrderId(order.getOrderId());
+//        }
+//        sos.insert(order);
+//        sods.insertBatch(orderdetails);
+//        //订单生成减去预计库存数量
+//        if(type == 0) {
+//            List<SaleOrderDetails> orderDetails=sods.queryById(order.getOrderId());
+//            for(SaleOrderDetails sod:orderDetails){
+//                bos.expectreduce(sod.getProductId(),sod.getDepot(),sod.getProductNum());
+//            }
+//        }
+//        return AjaxResponse.success(order.getOrderId());
+//    }
 
 }
