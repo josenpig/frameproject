2d4f6eb53a3d31daa369111fffb6e8b10ca3b292
lzh <3245633381@qq.com>
2021-06-17 19:30:48 +0800

    核销单修复bug

---
+++ src/main/java/com/xingji/frameproject/vo/CavConditionPageVo.java
@@ -0,0 +1,22 @@
+package com.xingji.frameproject.vo;
+
+import lombok.Data;
+
+/***
+ * @author: 顾渊白
+ * @date: 2021/6/17 19:03
+ * @version 1.0
+ */
+@Data
+public class CavConditionPageVo {
+    private String cavId;
+    private String cavBy;
+    private String orderTime;
+    private String cavType;
+    private String otimeState;
+    private String otimeEnd;
+    private String customer;
+    private String vendor;
+    private String founder;
+    private String approvalState;
+}
