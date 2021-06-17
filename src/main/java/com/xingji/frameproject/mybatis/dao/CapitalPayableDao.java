4e4382df50056a54bda0798a17afac37fb973033
lzh <3245633381@qq.com>
2021-06-17 15:25:29 +0800

    整合3

--- src/main/java/com/xingji/frameproject/mybatis/dao/CapitalPayableDao.java
+++ src/main/java/com/xingji/frameproject/mybatis/dao/CapitalPayableDao.java
@@ -1,16 +1,16 @@
 package com.xingji.frameproject.mybatis.dao;
 
 import com.xingji.frameproject.mybatis.entity.CapitalPayable;
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 /**
  * (CapitalPayable)表数据库访问层
  *
- * @author protagonist
- * @since 2021-06-17 11:19:05
+ * @author makejava
+ * @since 2021-06-17 15:23:32
  */
 @Mapper
 public interface CapitalPayableDao {
@@ -21,14 +21,17 @@ public interface CapitalPayableDao {
      * @param deliveryId 主键
      * @return 实例对象
      */
-    CapitalPayable selectById(String deliveryId);
+    CapitalPayable queryById(String deliveryId);
 
     /**
-     * 查询全部
+     * 查询指定行数据
      *
+     * @param offset 查询起始位置
+     * @param limit  查询条数
      * @return 对象列表
      */
-    List<CapitalPayable> selectAll();
+    List<CapitalPayable> queryAllByLimit(@Param("offset") int offset, @Param("limit") int limit);
+
 
     /**
      * 通过实体作为筛选条件查询
@@ -36,7 +39,7 @@ public interface CapitalPayableDao {
      * @param capitalPayable 实例对象
      * @return 对象列表
      */
-    List<CapitalPayable> selectList(CapitalPayable capitalPayable);
+    List<CapitalPayable> queryAll(CapitalPayable capitalPayable);
 
     /**
      * 新增数据
@@ -47,12 +50,20 @@ public interface CapitalPayableDao {
     int insert(CapitalPayable capitalPayable);
 
     /**
-     * 批量新增
+     * 批量新增数据（MyBatis原生foreach方法）
      *
-     * @param capitalPayables 实例对象的集合
+     * @param entities List<CapitalPayable> 实例对象列表
      * @return 影响行数
      */
-    int batchInsert(List<CapitalPayable> capitalPayables);
+    int insertBatch(@Param("entities") List<CapitalPayable> entities);
+
+    /**
+     * 批量新增或按主键更新数据（MyBatis原生foreach方法）
+     *
+     * @param entities List<CapitalPayable> 实例对象列表
+     * @return 影响行数
+     */
+    int insertOrUpdateBatch(@Param("entities") List<CapitalPayable> entities);
 
     /**
      * 修改数据
@@ -70,10 +81,5 @@ public interface CapitalPayableDao {
      */
     int deleteById(String deliveryId);
 
-    /**
-     * 查询总数据数
-     *
-     * @return 数据总数
-     */
-    int count();
 }
+
--- src/main/java/com/xingji/frameproject/mybatis/entity/CapitalPayable.java
+++ src/main/java/com/xingji/frameproject/mybatis/entity/CapitalPayable.java
@@ -3,17 +3,14 @@ package com.xingji.frameproject.mybatis.entity;
 import java.util.Date;
 import java.io.Serializable;
 
-import lombok.Data;
-
 /**
  * (CapitalPayable)实体类
  *
  * @author makejava
- * @since 2021-06-17 11:19:04
+ * @since 2021-06-17 15:23:31
  */
-@Data
 public class CapitalPayable implements Serializable {
-    private static final long serialVersionUID = -35489164059059817L;
+    private static final long serialVersionUID = -20948961804988009L;
     /**
      * 单据编号
      */
@@ -62,4 +59,102 @@ public class CapitalPayable implements Serializable {
      * 最后付款时间
      */
     private Date lastCollectionTime;
+
+
+    public String getDeliveryId() {
+        return deliveryId;
+    }
+
+    public void setDeliveryId(String deliveryId) {
+        this.deliveryId = deliveryId;
+    }
+
+    public Date getDeliveryTime() {
+        return deliveryTime;
+    }
+
+    public void setDeliveryTime(Date deliveryTime) {
+        this.deliveryTime = deliveryTime;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public String getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(String buyer) {
+        this.buyer = buyer;
+    }
+
+    public Double getPayables() {
+        return payables;
+    }
+
+    public void setPayables(Double payables) {
+        this.payables = payables;
+    }
+
+    public Double getPaid() {
+        return paid;
+    }
+
+    public void setPaid(Double paid) {
+        this.paid = paid;
+    }
+
+    public Double getUnpaid() {
+        return unpaid;
+    }
+
+    public void setUnpaid(Double unpaid) {
+        this.unpaid = unpaid;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public String getFounder() {
+        return founder;
+    }
+
+    public void setFounder(String founder) {
+        this.founder = founder;
+    }
+
+    public Integer getCaseState() {
+        return caseState;
+    }
+
+    public void setCaseState(Integer caseState) {
+        this.caseState = caseState;
+    }
+
+    public String getPaymentRemark() {
+        return paymentRemark;
+    }
+
+    public void setPaymentRemark(String paymentRemark) {
+        this.paymentRemark = paymentRemark;
+    }
+
+    public Date getLastCollectionTime() {
+        return lastCollectionTime;
+    }
+
+    public void setLastCollectionTime(Date lastCollectionTime) {
+        this.lastCollectionTime = lastCollectionTime;
+    }
+
 }
