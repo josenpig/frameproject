c0a5f8c4ae8f598a8267418e28617769ea26b3eb
LAPTOP-AMPEGKR5\lenovo <1104804222@qq.com>
2021-06-24 19:11:40 +0800

    1

--- src/main/java/com/xingji/frameproject/mybatis/dao/ReportFormDao.java
+++ src/main/java/com/xingji/frameproject/mybatis/dao/ReportFormDao.java
@@ -1,6 +1,7 @@
 package com.xingji.frameproject.mybatis.dao;
 
 import com.xingji.frameproject.vo.FundAccountsStatisticsVo;
+import com.xingji.frameproject.vo.ProductInventoryVo;
 import io.lettuce.core.dynamic.annotation.Param;
 import lombok.Data;
 import org.apache.ibatis.annotations.Mapper;
@@ -11,7 +12,7 @@ import java.util.List;
 @Mapper
 public interface ReportFormDao {
     /**
-     * 资金账户汇总表
+     * 资金账户汇总表 根据实体类中的条件查询
      * @param fundAccountsStatisticsVo
      * @return
      */
@@ -29,4 +30,12 @@ public interface ReportFormDao {
      */
     Double fundAllPsum(Date startTime,Date endTime);
 
+    /**************************/
+
+    /**
+     * 所有产品库存 根据实体类中的条件查询
+     * @param productInventoryVo
+     * @return
+     */
+    List<ProductInventoryVo> findAllProductInventoryVo(ProductInventoryVo productInventoryVo);
 }
